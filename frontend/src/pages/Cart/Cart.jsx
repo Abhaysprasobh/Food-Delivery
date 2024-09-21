9d41d082c911eccb7a0d3b3ccf4ac9aa86163eb4
Abhaysprasobh <abhay.s.prasobh@gmail.com>
2024-09-21 11:50:43 +0530

    navbar created using react

---
+++ frontend/src/pages/Cart/Cart.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Cart.css'
+import { assets } from '../../assets/assets'
+
+const Cart = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Cart
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+import { assets } from '../../assets/assets'
+
+const Home = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Home
