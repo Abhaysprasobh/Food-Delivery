c93e5123ef1ba6ab124f72de1678a2012b91ceea
Abhaysprasobh <abhay.s.prasobh@gmail.com>
2024-09-21 13:59:52 +0530

    added header

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className='header-contents'>
+            <h2>Order your favourite food here</h2>
+            <p>We're here to help you find the perfect meal With secure payments, fast shipping, and top-notch customer service, shopping has never been easier</p>
+            <button>View menu</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
