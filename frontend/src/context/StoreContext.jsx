4c205e96d57fec128b1e5eb875e2e9d2ccd6f85b
Abhaysprasobh <abhay.s.prasobh@gmail.com>
2024-09-22 09:05:22 +0530

    added a gallery section of the menu

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,2 +1,19 @@
+import { createContext } from "react";
+import { food_list } from "../assets/assets";
+export const StoreContext = createContext(null)
 
-export const StoreContext = create
+const StoreContextProvider = (props) => {
+    
+
+
+    const contextValue = {
+        food_list
+    }
+    return (
+        <StoreContext.Provider value = { contextValue }>
+            {props.children}
+        </StoreContext.Provider>
+    )
+}
+
+export default StoreContextProvider;
